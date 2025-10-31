aa4d171bb5009fcd96ed8ff4c2930e23cda9492e
Maxson Almeida <maxsonferovante@gmail.com>
2025-10-31 11:20:05 -0300

    fix: update explorer URL extraction to use nested blockchain structure in solana_response

--- certified_builder/solana_explorer_url.py
+++ certified_builder/solana_explorer_url.py
@@ -1,6 +1,6 @@
 def extract_solana_explorer_url(solana_response: dict) -> str:
     # alteração: função isolada para extrair a URL do explorer da resposta do serviço
-    explorer_url = solana_response.get("explorer_url", "")
+    explorer_url = solana_response.get("blockchain", {}).get("explorer_url", "")
     return explorer_url
 
 
